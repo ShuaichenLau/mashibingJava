d8c1a5ac93d8a1878c5842ae18098d3fb1fc5387
shuaichenlau <68754807@qq.com>
2020-07-07 20:12:44 +0800

    枚举类保证单例

---
+++ JavaParent/DesignModelDemo/src/main/java/com/alice/singleton/Singleton04.java
@@ -0,0 +1,40 @@
+package com.alice.singleton;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+/**
+ * 枚举类实现
+ * 2020-7-7 20:06:18
+ *
+ * @author liusc
+ */
+public class Singleton04 {
+
+    private Singleton04() {
+    }
+
+    private enum Singleton04Enum {
+        INSTANCE;
+        private final Singleton04 instance;
+
+        Singleton04Enum() {
+            instance = new Singleton04();
+        }
+
+        private Singleton04 getInstance() {
+            return instance;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton04Enum.INSTANCE.getInstance());
+                }
+            }).start();
+        }
+    }
+}
