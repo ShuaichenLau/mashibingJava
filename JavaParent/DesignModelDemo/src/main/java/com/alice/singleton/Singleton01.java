fd0f87d3413043fbed612ee19735b945a6277d66
shuaichenlau <68754807@qq.com>
2020-07-07 14:53:45 +0800

    Singleton model demo

---
+++ JavaParent/DesignModelDemo/src/main/java/com/alice/singleton/Singleton01.java
@@ -0,0 +1,31 @@
+package com.alice.singleton;
+
+/**
+ * 饿汉式 单例模式
+ *
+ * @author liusc
+ * 2020-7-7 14:38:41
+ */
+public class Singleton01 {
+
+    private static Singleton01 singleton01 = new Singleton01();
+
+    private Singleton01() {
+    }
+
+    public static Singleton01 getSingleton01() {
+        return singleton01;
+    }
+
+    public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    System.out.println(Singleton01.getSingleton01().toString());
+                }
+            }
+        }).start();
+    }
+}
---
+++ JavaParent/DesignModelDemo/src/main/java/com/alice/singleton/Singleton02.java
@@ -0,0 +1,38 @@
+package com.alice.singleton;
+
+/**
+ * 懒汉式 单例模式
+ * 2020-7-7 14:41:18
+ *
+ * @author liusc
+ */
+public class Singleton02 {
+
+    private static volatile Singleton02 singleton02;
+
+    private Singleton02() {
+    }
+
+    public static Singleton02 getInstance() {
+        if (singleton02 == null) {
+            synchronized (Singleton02.class) {
+                if (singleton02 == null) {
+                    singleton02 = new Singleton02();
+                }
+            }
+        }
+        return singleton02;
+    }
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton02.getInstance().toString());
+                }
+            }).start();
+        }
+    }
+}
