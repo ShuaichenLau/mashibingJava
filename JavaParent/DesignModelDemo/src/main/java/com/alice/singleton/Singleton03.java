230c538de0b280889d15d86929ca2a8f3f1bf68e
shuaichenlau <68754807@qq.com>
2020-07-07 20:12:30 +0800

    静态内部类保证单例

---
+++ JavaParent/DesignModelDemo/src/main/java/com/alice/singleton/Singleton03.java
@@ -0,0 +1,24 @@
+package com.alice.singleton;
+
+/**
+ * 静态内部类
+ *
+ * @author liusc
+ * 2020-7-7 20:02:36
+ */
+public class Singleton03 {
+
+    private Singleton03() {
+    }
+
+
+    private static class InnerSingletonClass {
+        private static final Singleton03 singleton03 = new Singleton03();
+    }
+
+    public static Singleton03 getInstance() {
+        return InnerSingletonClass.singleton03;
+    }
+
+
+}
