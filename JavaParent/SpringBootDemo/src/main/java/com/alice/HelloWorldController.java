d4925b132172767013a1055de1655cc87a48b378
shuaichenlau <68754807@qq.com>
2020-07-13 15:49:16 +0800

    logger level

--- JavaParent/SpringBootDemo/src/main/java/com/alice/HelloWorldController.java
+++ JavaParent/SpringBootDemo/src/main/java/com/alice/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.alice;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +9,18 @@ import java.util.Date;
 
 @RestController
 public class HelloWorldController {
+    public static Log log = LogFactory.getLog(HelloWorldController.class);
+
 
     @RequestMapping("/")
     public String index(){
-        return "helloWorld docker" + new Date().getTime();
+        log.info("com.alice.HelloWorldController.index");
+        return "helloWorld docker  " + new Date().toString();
+    }
+
+    @RequestMapping("/info")
+    public String info(){
+        log.info("com.alice.HelloWorldController.info");
+        return "helloWorld liusc  " + new Date().toString();
     }
 }
