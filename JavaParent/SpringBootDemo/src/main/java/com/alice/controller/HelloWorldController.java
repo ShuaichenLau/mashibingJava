a869b57658811b822a5463d7a919a5efe2354813
shuaichenlau <68754807@qq.com>
2020-07-14 17:35:59 +0800

    create controller package

--- JavaParent/SpringBootDemo/src/main/java/com/alice/controller/HelloWorldController.java
+++ JavaParent/SpringBootDemo/src/main/java/com/alice/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.alice;
+package com.alice.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -7,20 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+/**
+ * index
+ * @author liusc
+ */
 @RestController
 public class HelloWorldController {
     public static Log log = LogFactory.getLog(HelloWorldController.class);
 
-
     @RequestMapping("/")
     public String index(){
-        log.info("com.alice.HelloWorldController.index");
+        log.info("com.alice.controller.HelloWorldController.index");
         return "helloWorld docker  " + new Date().toString();
     }
 
     @RequestMapping("/info")
     public String info(){
-        log.info("com.alice.HelloWorldController.info");
+        log.info("com.alice.controller.HelloWorldController.info");
         return "helloWorld liusc  " + new Date().toString();
     }
 }
