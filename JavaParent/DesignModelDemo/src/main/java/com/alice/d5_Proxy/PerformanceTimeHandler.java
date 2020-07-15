3a645cb02d5197a546f82f7e74657fc970bcb02d
shuaichenlau <68754807@qq.com>
2020-07-15 17:34:41 +0800

    add cglib asm support

--- JavaParent/DesignModelDemo/src/main/java/com/alice/d5_Proxy/PerformanceTimeHandler.java
+++ JavaParent/DesignModelDemo/src/main/java/com/alice/d5_Proxy/PerformanceTimeHandler.java
@@ -7,6 +7,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class PerformanceTimeHandler implements InvocationHandler {
+
     private Tank tank;
 
     private Log log = LogFactory.get(this.getClass());
@@ -19,6 +20,7 @@ public class PerformanceTimeHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         long startTime = System.currentTimeMillis();
         log.info("method start..." + method.getName());
+        // 被代理的对象 tank
         method.invoke(tank, args);
         log.info("method end..." + method.getName());
         log.info("function耗时 ==> " + (System.currentTimeMillis() - startTime) + " ms");
