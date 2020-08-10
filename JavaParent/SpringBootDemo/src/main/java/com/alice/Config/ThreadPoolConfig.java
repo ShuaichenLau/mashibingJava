e80e8dbd4cb33117a29c7facba70967a330ff6f1
shuaichenlau <68754807@qq.com>
2020-08-10 11:41:05 +0800

    异步线程池

---
+++ JavaParent/SpringBootDemo/src/main/java/com/alice/Config/ThreadPoolConfig.java
@@ -0,0 +1,35 @@
+package com.alice.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean("asyncServiceExecutor")
+    public Executor asyncServiceExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        int count = Runtime.getRuntime().availableProcessors();
+        //配置核心线程数
+        executor.setCorePoolSize(count);
+        //配置最大线程数
+        executor.setMaxPoolSize(count * 2);
+        //配置队列大小
+        executor.setQueueCapacity(100);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("async-service-");
+
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
+}
