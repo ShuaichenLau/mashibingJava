c749b7ca00de27c10f08609c42b4137eab187174
shuaichenlau <68754807@qq.com>
2020-09-17 01:02:06 +0800

    JDK8 Stream 并行流

---
+++ JavaParent/DesignModelDemo/src/main/java/com/threads/demo_v3/ThreadJDK8Demo.java
@@ -0,0 +1,27 @@
+package com.threads.demo_v3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ThreadJDK8Demo {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 200000; i++) {
+            strings.add(UUID.randomUUID().toString());
+        }
+
+        long startTime = System.currentTimeMillis();
+        strings.parallelStream().forEach(str -> {
+            System.out.println(str);
+        });
+//        for (String string : strings) {
+//            System.out.println(string);
+//        }
+
+
+        System.err.println(System.currentTimeMillis() - startTime + " ms ");
+
+    }
+}
